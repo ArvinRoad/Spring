58d2c3453f6482bad42b4e8309fe33a02c445786
ArvinRoad <53329456+ArvinRoad@users.noreply.github.com>
2021-01-23 00:03:31 +0800

    万能的代理类

---
+++ Spring-08-proxy/src/main/java/com/Demo/demo04/ProxyInvocationHandler.java
@@ -0,0 +1,23 @@
+package com.Demo.demo04;
+
+import java.lang.reflect.Proxy;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+//万能的代理类
+public class ProxyInvocationHandler implements InvocationHandler {
+
+    private Object target;
+    public void setTarget(Object target) {
+        this.target = target;
+    }
+
+    public Object getProxy(){
+        return Proxy.newProxyInstance(this.getClass().getClassLoader(),target.getClass().getInterfaces(),this);
+    }
+
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        Object result = method.invoke(target,args);
+        return result;
+    }
+}
